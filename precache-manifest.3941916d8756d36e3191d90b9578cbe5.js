2d74d1914c5e3ec79aad14192b47995abf07bfde
Diego123123123 <doms1369@gmail.com>
2020-10-21 19:15:50 -0400

    Updates

---
+++ precache-manifest.3941916d8756d36e3191d90b9578cbe5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5d0ceeec1fee16d94e7dea7bd2a7f4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eba5a138338a205b0468",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "b261bc007df6484a4111",
+    "url": "./static/js/2.c18c439e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c18c439e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eba5a138338a205b0468",
+    "url": "./static/js/main.1b5ffa1f.chunk.js"
+  },
+  {
+    "revision": "207abb033ddc8dbb9328",
+    "url": "./static/js/runtime-main.8a830bd8.js"
+  }
+]);
